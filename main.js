5f09b3b7a0ad38311c73a51d592b2aa31de32a82
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-09-14 11:33:42 +0200

    liczenie

---
+++ main.js
@@ -0,0 +1,14 @@
+var lubie = "Uwielbiam JavaScript" ;
+var jestem = "Jestem świetnym programistą";
+
+function omnie (lubie,jestem){
+    var result 
+    if (lubie>jestem){
+            console.log(lubie);
+        }else{
+            console.log(jestem);
+        }
+    return result;
+}
+
+omnie();
