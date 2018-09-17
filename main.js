71dcadb5179dabf172eab90a180aa0eda7d5c99a
js-zadanie23 <42982482+GosiaSamosia@users.noreply.github.com>
2018-09-17 14:58:20 +0200

    Update main.js
    poprawka

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+var lubie = "Uwielbiam JavaScript";
+var jestem = "Jestem świetnym programistą";
+
+function omnie(lubie, jestem) {
+    if (lubie>jestem===true) {
+        return lubie;
+    } else {
+        return jestem;
+    }
+}
+
+console.log(omnie(lubie, jestem));
+
+/*
+var lubie = "Uwielbiam JavaScript";
+var jestem = "Jestem świetnym programistą";
+
+var result;
+
+function omnie(lubie, jestem) { 
+    var wieksza = lubie + jestem;
+    if (lubie>jestem) {
+        return lubie;
+    }else{
+        return jestem;
+    }
+return wieksza;
+}
+
+console.log(omnie(lubie, jestem));
+
+starawersja:
 var lubie = "Uwielbiam JavaScript" ;
 var jestem = "Jestem świetnym programistą";
 
@@ -12,3 +44,4 @@ function omnie (lubie,jestem){
 }
 
 omnie();
+*/
