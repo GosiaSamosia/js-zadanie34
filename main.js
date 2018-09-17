f8b17b2003db7b6a03a6081c0dbb7cac97f6d647
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-09-17 14:45:58 +0200

    poprawka

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-var lubie = "Uwielbiam JavaScript" ;
+var lubie = "Uwielbiam JavaScript";
 var jestem = "Jestem świetnym programistą";
 
-function omnie (lubie,jestem){
-    var result 
-    if (lubie>jestem){
-            console.log(lubie);
-        }else{
-            console.log(jestem);
-        }
-    return result;
+function omnie(lubie, jestem) {
+    if (lubie>jestem===true) {
+        return lubie;
+    } else {
+        return jestem;
+    }
 }
 
-omnie();
+console.log(omnie(lubie, jestem));
+
+/*
+var lubie = "Uwielbiam JavaScript";
+var jestem = "Jestem świetnym programistą";
+
+var result;
+
+function omnie(lubie, jestem) { 
+    var wieksza = lubie + jestem;
+    if (lubie>jestem) {
+        return lubie;
+    }else{
+        return jestem;
+    }
+return wieksza;
+}
+
+console.log(omnie(lubie, jestem));
+*/
