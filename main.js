85c9a65a63cf15d405ab44f248fe7828de982e23
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-09-18 11:37:32 +0200

    dodanie length

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var lubie = "Uwielbiam JavaScript";
 var jestem = "Jestem świetnym programistą";
 
 function omnie(lubie, jestem) {
-    if (lubie>jestem===true) {
+    if (lubie.length>jestem.length===true) {
         return lubie;
     } else {
         return jestem;
@@ -10,22 +10,3 @@ function omnie(lubie, jestem) {
 }
 
 console.log(omnie(lubie, jestem));
-
-/*
-var lubie = "Uwielbiam JavaScript";
-var jestem = "Jestem świetnym programistą";
-
-var result;
-
-function omnie(lubie, jestem) { 
-    var wieksza = lubie + jestem;
-    if (lubie>jestem) {
-        return lubie;
-    }else{
-        return jestem;
-    }
-return wieksza;
-}
-
-console.log(omnie(lubie, jestem));
-*/
